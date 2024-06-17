d17475d8c832df85939b820fb16a9904daeca10e
오상훈 <ph38230119@gmail.com>
2024-06-18 00:43:04 +0900

    [Feat] Station CRUD API 수정 스트림 내부에 ResponseEntity를 넣지 않게 수정

--- src/main/java/com/ddd/station/controller/StationController.java
+++ src/main/java/com/ddd/station/controller/StationController.java
@@ -4,17 +4,16 @@ import com.ddd.station.model.StationCreate;
 import com.ddd.station.model.StationResponse;
 import com.ddd.station.model.StationUpdate;
 import com.ddd.station.service.StationService;
-import com.ddd.station.util.Util;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -24,40 +23,41 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class StationController {
 
-    private final StationService stationService;
+	private final StationService stationService;
 
-    @GetMapping
-    public Mono<String> healthCheck() {
-        return Mono.just("OK");
-    }
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public Mono<String> healthCheck() {
+		return Mono.just("OK");
+	}
 
-    @GetMapping("/station/{stationId}")
-    public Mono<StationResponse> getStation(@PathVariable String stationId) {
-        return stationService.getStationById(stationId)
-                .map(StationResponse::convert);
-    }
+	@GetMapping("/station/{stationId}")
+	@ResponseStatus(HttpStatus.OK)
+	public Mono<StationResponse> getStation(@PathVariable String stationId) {
+		return stationService.getStationById(stationId).map(StationResponse::convert);
+	}
 
-    @GetMapping("/stations")
-    public Flux<StationResponse> getStations() {
-        return stationService.getStations()
-                .map(StationResponse::convert);
-    }
+	@GetMapping("/stations")
+	@ResponseStatus(HttpStatus.OK)
+	public Flux<StationResponse> getStations() {
+		return stationService.getStations().map(StationResponse::convert);
+	}
 
-    @PostMapping("/station")
-    public Mono<ResponseEntity<Void>> createStation(@RequestBody StationCreate stationCreate) {
-        return stationService.createStation(stationCreate)
-                .map(id -> ResponseEntity.created(Util.createURI(id)).build());
-    }
+	@PostMapping("/station")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Mono<Void> createStation(@RequestBody StationCreate stationCreate) {
+		return stationService.createStation(stationCreate).then();
+	}
 
-    @PutMapping("/station")
-    public Mono<ResponseEntity<Void>> updateStation(@RequestBody StationUpdate stationUpdate) {
-        return stationService.updateStation(stationUpdate)
-                .then(Mono.just(ResponseEntity.noContent().build()));
-    }
+	@PutMapping("/station")
+	@ResponseStatus(HttpStatus.OK)
+	public Mono<Void> updateStation(@RequestBody StationUpdate stationUpdate) {
+		return stationService.updateStation(stationUpdate).then();
+	}
 
-    @DeleteMapping("/station/{stationId}")
-    public Mono<ResponseEntity<Void>> deleteStation(@PathVariable String stationId) {
-        return stationService.deleteStationById(stationId)
-                .then(Mono.just(ResponseEntity.status(HttpStatus.NO_CONTENT).build()));
-    }
+	@DeleteMapping("/station/{stationId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public Mono<Void> deleteStation(@PathVariable String stationId) {
+		return stationService.deleteStationById(stationId).then();
+	}
 }
